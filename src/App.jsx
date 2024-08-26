e32f12f18dd924ed530605a5ec007176e44fc046
Lionel <lpei1900@gmail.com>
2024-08-26 16:18:49 -0400

    Updated UsePokemon Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
-import createRandomPairs from './usePokemon.jsx';
-import GameBoard from './components/GameBoard.jsx';
+
+
+
+import React, { useEffect } from 'react';
+import usePokemon from './usePokemon.jsx';
 
 function App() {
+  const { pokemons, createRandomPairs } = usePokemon();
+
+  useEffect(() => {
+    createRandomPairs(5);
+  }, []);
+
+
   return (
-    <>
-      <h1>Hello</h1>
-      <GameBoard ></GameBoard>
-    </>
+    <div>
+      <h1>Random Pokémon</h1>
+      {pokemons.map((pokemon, index) => (
+        <div key={index}>
+          <img src={pokemon.image} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
--- src/usePokemon.jsx
+++ src/usePokemon.jsx
@@ -1,47 +1,131 @@
 import { useState, useEffect } from 'react';
 
-const getPokemon = async ({pokeId}) => {
-  try {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
-    if (!res.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const { name, sprites } = await res.json();
-    const image = sprites.front_default;
-    return { name, image, pokeId};
-  } catch (error) {
-    console.error('Error fetching Pokémon data:', error);
-    return null;
-  }
-};
+export default function usePokemons() {
+  const [pokemons, setPokemons] = useState([]);
+  const NUM_OF_POKEMONS= 1025;
 
-const getRandomPokemons = async (amount) => {
-  let pokemonArray = [];
+  const getPokemon = async ({pokeId}) => {
+    try {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const { name, sprites } = await res.json();
+      const image = sprites.front_default;
+      return { name, image, pokeId};
+    } catch (error) {
+      console.error('Error fetching Pokémon data:', error);
+      return null;
+    }
+  };
   
-  while (pokemonArray.length < amount) {
-    const randomId = Math.floor(Math.random() * 1025) + 1;
-    const isDuplicateId = pokemonArray.find(({ id }) => id === randomId);
-    if (!isDuplicateId) {
-      pokemonArray.push({id: randomId});
+  const getRandomPokemons = async (amount) => {
+    let pokemonArray = [];
+    
+    while (pokemonArray.length < amount) {
+      const randomId = Math.floor(Math.random() * NUM_OF_POKEMONS) + 1;
+      const isDuplicateId = pokemonArray.find(({ id }) => id === randomId);
+      if (!isDuplicateId) {
+        pokemonArray.push({id: randomId});
+      }
     }
-  }
-  return await Promise.all(pokemonArray.map(({id}) => getPokemon({pokeId: id})));
-};
+    return await Promise.all(pokemonArray.map(({id}) => getPokemon({pokeId: id})));
+  };
+  
+  // Fisher-Yates Shuffle
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  
+  const createRandomPairs = async (amount) => {
+    const pokemonArray = await getRandomPokemons(amount);
+    let pokemonPairs = [...pokemonArray, ...pokemonArray];
+    const randomPairs = shuffleArray(pokemonPairs);
+    setPokemons(randomPairs);
+    // return randomPairs;
+  };
+  
+  return { pokemons, getRandomPokemons, createRandomPairs, setPokemons };
+}
 
-// Fisher-Yates Shuffle
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
 
-const createRandomPairs = async (amount) => {
-  const pokemonArray = await getRandomPokemons(amount);
-  let pokemonPairs = [...pokemonArray, ...pokemonArray];
-  const randomPairs = shuffleArray(pokemonPairs);
-  return randomPairs;
-};
 
-export default createRandomPairs;
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from 'react';
+
+
+
+// const getPokemon = async ({pokeId}) => {
+//   try {
+//     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+//     if (!res.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     const { name, sprites } = await res.json();
+//     const image = sprites.front_default;
+//     return { name, image, pokeId};
+//   } catch (error) {
+//     console.error('Error fetching Pokémon data:', error);
+//     return null;
+//   }
+// };
+
+// const getRandomPokemons = async (amount) => {
+//   let pokemonArray = [];
+  
+//   while (pokemonArray.length < amount) {
+//     const randomId = Math.floor(Math.random() * 1025) + 1;
+//     const isDuplicateId = pokemonArray.find(({ id }) => id === randomId);
+//     if (!isDuplicateId) {
+//       pokemonArray.push({id: randomId});
+//     }
+//   }
+//   return await Promise.all(pokemonArray.map(({id}) => getPokemon({pokeId: id})));
+// };
+
+// // Fisher-Yates Shuffle
+// const shuffleArray = (array) => {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// };
+
+// // const createRandomPairs = async (amount) => {
+// //   const pokemonArray = await getRandomPokemons(amount);
+// //   let pokemonPairs = [...pokemonArray, ...pokemonArray];
+// //   const randomPairs = shuffleArray(pokemonPairs);
+// //   return randomPairs;
+// // };
+
+// const createRandomPairs = (amount) => {
+//   return getRandomPokemons(amount)
+//     .then(pokemonArray => {
+//       let pokemonPairs = [...pokemonArray, ...pokemonArray];
+//       const randomPairs = shuffleArray(pokemonPairs);
+//       return randomPairs;
+//     });
+// };
+
+// export default createRandomPairs;
+
+
+
+
+
+
+
+
