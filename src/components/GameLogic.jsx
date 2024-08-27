bb2d2d97f0645b85ab14edc0f62ec7e3f8c0b4b5
Lionel <lpei1900@gmail.com>
2024-08-27 13:07:17 -0400

    Added delay after finding a match

--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -1,90 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import GameBoard from './GameBoard.jsx';
-// import usePokemon from '../usePokemon.jsx';
-
-// function MemoryGame() {
-//   const { pokemons, createRandomPairs } = usePokemon();
-//   const [cards, setCards] = useState([]);
-//   const [flippedCards, setFlippedCards] = useState([]);
-//   const [matchedCards, setMatchedCards] = useState([]);
-
-//   // Create Pokemon pairs
-//   useEffect(() => {
-//     createRandomPairs(50);
-//   }, []);
-
-//   // Create cards array when pokemons are updated
-//   useEffect(() => {
-//     if (pokemons.length > 0) {
-//       const cardsArray = pokemons.map((item, index) => ({
-//         ...item,
-//         id: index,
-//         state: 'back'
-//       }));
-//       setCards(cardsArray);
-//     }
-//   }, [pokemons]);
-
-
-
-// //   useEffect(() => {
-// //     if (matchedCards.length === cards.length) {
-// //       alert('Congratulations! You have matched all the cards.');
-// //       resetGame();
-// //     }
-// //   }, [matchedCards]);
-
-// //   const resetGame = () => {
-// //     createRandomPairs(10); // Reset with a new set of cards
-// //     setFlippedCards([]);
-// //     setMatchedCards([]);
-// //   };
-
-//   const handleCardClick = (id) => {
-//     console.log('hello');
-//     const clickedCard = cards[id];
-
-//     if (flippedCards.length === 2 || clickedCard.state !== 'back') {
-//       return;
-//     }
-
-//     const newCards = cards.map(card => card.id === id ? { ...card, state: 'front' } : card);
-//     setCards(newCards);
-//     setFlippedCards([...flippedCards, id]);
-
-//     if (flippedCards.length === 1) {
-//       const firstCard = cards.find(card => card.id === flippedCards[0]);
-//       if (firstCard.name === clickedCard.name) { //Cards match
-//         setMatchedCards([...matchedCards, firstCard.id, clickedCard.id]);
-//         setFlippedCards([]);
-//         const updatedCards = cards.map(card => 
-//             card.id === id || card.id === firstCard.id ? { ...card, state: 'invisible' } : card
-//         );
-//         setCards(updatedCards);
-//       } 
-//     else {
-//     setTimeout(() => {
-//           setCards(cards.map(card => flippedCards.includes(card.id) || card.id === id ? { ...card, state: 'back' } : card));
-//           setFlippedCards([]);
-//         }, 1500);
-//       }
-//     }
-//   };
-
-
-
-//   return (
-//     <GameBoard
-//       cards={cards}
-//       onCardClick={handleCardClick}
-//     />
-//   );
-// }
-
-// export default MemoryGame;
-
-
-
 import React, { useState, useEffect } from 'react';
 import GameBoard from './GameBoard.jsx';
 import usePokemon from '../usePokemon.jsx';
@@ -94,6 +7,7 @@ function MemoryGame() {
   const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
   const [matchedCards, setMatchedCards] = useState([]);
+
 
   // Create Pokemon pairs
   useEffect(() => {
@@ -128,14 +42,18 @@ function MemoryGame() {
     if (flippedCards.length === 1) {
       const firstCard = cards.find(card => card.id === flippedCards[0]);
 
+      //If there is a match
       if (firstCard.name === clickedCard.name) {
         setMatchedCards(prev => [...prev, firstCard.id, clickedCard.id]);
         setFlippedCards([]);
 
-        setCards(prevCards => prevCards.map(card =>
-          card.id === id || card.id === firstCard.id
-            ? { ...card, state: 'invisible' } : card
-        ));
+        //Set cards to invisible
+        setTimeout(() => {
+          const updatedCards = newCards.map(card =>
+            card.id === id || card.id === firstCard.id ? { ...card, state: 'invisible' } : card
+          );
+          setCards(updatedCards);
+        }, 2000);
       } 
       else {
         setTimeout(() => {
@@ -145,7 +63,7 @@ function MemoryGame() {
               : card
           ));
           setFlippedCards([]);
-        }, 1500);
+        }, 2000);
       }
     }
   };
@@ -159,12 +77,3 @@ function MemoryGame() {
 }
 
 export default MemoryGame;
-
-
-
-//     setTimeout(() => {
-//           setCards(cards.map(card => flippedCards.includes(card.id) || card.id === id ? { ...card, state: 'back' } : card));
-//           setFlippedCards([]);
-//         }, 1500);
-//       }
-//     }
