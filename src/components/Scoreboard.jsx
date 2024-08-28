5a1269b98db8c21aa0e2f2fc1adac90642981057
Lionel <lpei1900@gmail.com>
2024-08-28 19:32:00 -0400

    Finished styling scoreboard component

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -4,9 +4,15 @@ import "../styles/Scoreboard.css";
 function Scoreboard({ moves, misses, score, scoreGoal }) {
   return (
   <div className="scoreboard">
-    <div className="moves-info">Moves: {moves}</div>
-    <div className="misses-info">Misses: {misses}</div>
-    <div className="score-info">Score: {score}/{scoreGoal}</div>
+    <div className="logo">
+      <h1 className="title">Pokémon</h1>
+      <h2 className="subheading">CARD MATCH GAME</h2>
+    </div>
+    <div className="info">
+      <div className="moves-info">Moves: {moves}</div>
+      <div className="misses-info">Misses: {misses}</div>
+      <div className="score-info">Score: {score}/{scoreGoal}</div>
+    </div>
   </div>
   );
 }
