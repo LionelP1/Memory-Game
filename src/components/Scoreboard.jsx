0bb74813a0bd68fa0c2f068dc9c81ef5912138ce
Lionel <lpei1900@gmail.com>
2024-08-29 09:53:48 -0400

    Made scoreboard better

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,17 +3,19 @@ import "../styles/Scoreboard.css";
 
 function Scoreboard({ moves, misses, score, scoreGoal, onQuit }) {
   return (
-  <div className="scoreboard">
-    <div className="logo" onClick={onQuit}>
-      <h1 className="title">Pokémon</h1>
-      <h2 className="subheading">CARD MATCH GAME</h2>
+    <div className="scoreboard-wrapper">
+      <div className="scoreboard">
+        <div className="logo" onClick={onQuit}>
+          <h1 className="title">Pokémon</h1>
+          <h2 className="subheading">CARD MATCH GAME</h2>
+        </div>
+        <div className="info">
+          <div className="moves-info">Moves: {moves}</div>
+          <div className="misses-info">Misses: {misses}</div>
+          <div className="score-info">Score: {score}/{scoreGoal}</div>
+        </div>
+      </div>
     </div>
-    <div className="info">
-      <div className="moves-info">Moves: {moves}</div>
-      <div className="misses-info">Misses: {misses}</div>
-      <div className="score-info">Score: {score}/{scoreGoal}</div>
-    </div>
-  </div>
   );
 }
 
