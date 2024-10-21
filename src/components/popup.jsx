06a9d082ea923bec71e985e2e73993f2e128e33b
Lionel <lpei1900@gmail.com>
2024-10-21 16:36:11 -0400

    Changed the header to h3

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -5,7 +5,7 @@ export default function Popup({ message, moves, misses, score, onRestart, onQuit
   return (
     <div className="popup">
       <div className="popup-content">
-        <h4>{message}</h4>
+        <h3>{message}</h3>
         <div className="trophy-img">🏆</div>
         <p>You played {moves} moves</p>
         <p>You missed {misses} moves</p>
